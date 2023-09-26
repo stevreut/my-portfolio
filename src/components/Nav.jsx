a054cd81c6f3b1065d486fee62778dfd4aa7b7c5
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-26 00:10:28 -0400

    nazarlik plus minor style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,6 +9,9 @@ import Navbar from './Navbar';
 export default function Nav() {
     // The Navbar component will render each of the Link elements in the links prop
     return (
+        <header>
+            <img src="/nazarlik.svg" width="75px" height="75px"></img>
+            <h1>Steve Reuterskiold</h1>
         <Navbar
             links={[
                 <Link key={1} className="nav-link text-light" to="/">
@@ -25,5 +28,6 @@ export default function Nav() {
                 </Link>,
             ]}
         />
+        </header>
     );
 }
