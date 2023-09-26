7f30a447bc8b6afbdd81a237f2616a04f24b9d8b
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-25 22:44:10 -0400

    placeholder pages installed and minimally working

---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+// Bringing in the required import from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+
+// TODO
+// TODO : About Me, Portfolio, Contact, and Resume - required links per assignment README
+// TODO
+
+export default function Nav() {
+    // The Navbar component will render each of the Link elements in the links prop
+    return (
+        <Navbar
+            links={[
+                <Link key={1} className="nav-link text-light" to="/">
+                    About Me
+                </Link>,
+                <Link key={2} className="nav-link text-light" to="/portfolio">
+                    Portfolio
+                </Link>,
+                <Link key={3} className="nav-link text-light" to="/contact">
+                    Contact Me
+                </Link>,
+                <Link key={4} className="nav-link text-light" to="/resume">
+                    Resum&eacute;
+                </Link>,
+            ]}
+        />
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+// import 'bootstrap/dist/css/bootstrap.min.css';  // TODO
+import './index.css';
+
+// TODO
+// TODO : About Me, Portfolio, Contact, and Resume - required links per assignment README
+// TODO
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import AboutPage from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ResumePage';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
