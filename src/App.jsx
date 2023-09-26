9831d7562934d006c5118e2b5bcc467aba9e5ab2
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-26 09:34:14 -0400

    rotate nazarlik, add tabKey, setTabKey (but not used yet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 function App() {
+  const [tabKey, setTabKey] = useState(1);
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
