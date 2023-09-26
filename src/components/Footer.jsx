e87a0c22589aba59029588a36d0325fa6abc58fa
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-25 23:02:53 -0400

    added Footer.jsx, strangeness with nav/navbar needs to be resolved

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            &copy; 2023 Steve Reuterskiold
+            {/* TODO - various logos */}
+        </footer>
+    );
+  }
+  
