5534d8494ca20c349e59822b5b81008c1190f165
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-26 21:53:42 -0400

    style nav links based on URL path, style taking shows but not taking effect yet pending CSS or framework work

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 // Bringing in the required import from 'react-router-dom'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 
 // TODO
@@ -8,22 +8,26 @@ import Navbar from './Navbar';
 
 export default function Nav() {
     // The Navbar component will render each of the Link elements in the links prop
+    const currentPagePath = useLocation().pathname;
+    const dormantPage = "nav-link text-light";
+    const activePage = dormantPage + " active";
+
     return (
         <header>
             <img src="/nazarlik.svg" width="75px" height="75px"></img>
             <h1>Steve Reuterskiold</h1>
         <Navbar
             links={[
-                <Link key={1} className="nav-link text-light" to="/">
+                <Link key={1} className={currentPagePath === "/" ? activePage : dormantPage} to="/">
                     About Me
                 </Link>,
-                <Link key={2} className="nav-link text-light" to="/portfolio">
+                <Link key={2} className={currentPagePath === "/portfolio" ? activePage : dormantPage} to="/portfolio">
                     Portfolio
                 </Link>,
-                <Link key={3} className="nav-link text-light" to="/contact">
+                <Link key={3} className={currentPagePath === "/contact" ? activePage : dormantPage} to="/contact">
                     Contact Me
                 </Link>,
-                <Link key={4} className="nav-link text-light" to="/resume">
+                <Link key={4} className={currentPagePath === "/resume" ? activePage : dormantPage} to="/resume">
                     Resum&eacute;
                 </Link>,
             ]}
