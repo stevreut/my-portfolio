2e52391387d8c5987098ce44eee7cdb0ac24f3e9
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-09-27 23:32:55 -0400

    style

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import ReactDOM from 'react-dom/client';
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';  // TODO
 import './index.css';
-
-// TODO
-// TODO : About Me, Portfolio, Contact, and Resume - required links per assignment README
-// TODO
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
 import App from './App';
